2990a728913ecc690b5dda9697615ad7da852f0d
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2022-01-24 19:54:28 -0600

    Create serial_read.py

---
+++ 0125/serial_read.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Thanks to The Robotics Back-End.
+https://roboticsbackend.com/raspberry-pi-arduino-serial-communication/
+"""
+import serial
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.reset_input_buffer()
+
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
